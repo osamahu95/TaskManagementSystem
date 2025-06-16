4170ac4ff02d817f4841e7e804fb83569468ec01
osamascience96 <osamahu96@gmail.com>
2025-06-16 16:43:15 +0500

    Day_One_Work_with_basic_project_setup

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Domain.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Data
+{
+    public class AppDbContext: DbContext
+    {
+        public AppDbContext(DbContextOptions options): base(options)
+        {
+        }
+
+        public DbSet<Domain.Models.Task> Tasks { get; set; }
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
---
+++ Domain/Models/Task.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Models
+{
+    public class Task
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public bool IsCompleted { get; set; }
+        // Navigation Properties
+        public int UserId { get; set; }
+        public User AssignedUser { get; set; }
+    }
+}
