7456ff94b04e99759c104503e6257437f0b3d7a5
osamascience96 <osamahu96@gmail.com>
2025-06-19 17:46:52 +0500

    Usage of Deligates

---
+++ Infrastructure/Events/TasksEvent.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskApi.Infrastructure.Events
+{
+    public class TasksEvent
+    {
+        public static void HandleTaskCreated(Domain.Models.Task task)
+        {
+            // Logic to handle task creation event
+            Debug.WriteLine($"Task Created: {task.Title} at {DateTime.Now}.");
+        }
+    }
+}
--- Infrastructure/Services/TaskService.cs
+++ Infrastructure/Services/TaskService.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using TaskApi.Domain.Interface.Validators;
+using TaskApi.Infrastructure.Events;
 
 namespace TaskApi.Infrastructure.Services
 {
@@ -15,10 +16,16 @@ namespace TaskApi.Infrastructure.Services
         private readonly IUnitOfWork _unitOfWork;
         private readonly IEnumerable<ITaskValidator> _taskValidators;
 
+        public delegate void TaskCreatedHandler(Domain.Models.Task task);
+        public event TaskCreatedHandler TaskCreated;
+
         public TaskService(IUnitOfWork unitOfWork, IEnumerable<ITaskValidator> taskValidators)
         {
             _unitOfWork = unitOfWork;
             _taskValidators = taskValidators;
+
+            // Subscribe to the TaskCreated event if needed
+            TaskCreated += TasksEvent.HandleTaskCreated; // Example of subscribing to the event
         }
 
         public async Task<Domain.Models.Task> CreateTask(TaskInputViewModel taskInputViewModel)
@@ -39,6 +46,10 @@ namespace TaskApi.Infrastructure.Services
 
             await _unitOfWork.TaskRepository.Add(task);
             await _unitOfWork.SaveChanges();
+
+            // Trigger the event after task creation
+            TaskCreated?.Invoke(task);
+
             return task;
         }
 
